3d1eace84e74fb831c309b21a4194b6cdc22e332
PdoubleU <piotrwitasik1988@gmail.com>
2021-03-29 20:32:01 +0200

    initial commit;

---
+++ dist/script.js
@@ -0,0 +1,16 @@
+/*function below fetches random pictures from the outer website and put them inside elements choosen and stored in a variable named placeholder*/
+function renderItem(){
+    let placeholder = document.querySelectorAll('.placeholder');
+    for (let i=0; i < placeholder.length; i++) {
+        fetch(`https://source.unsplash.com/300x300/?chessboard`)
+        .then((response) => {
+            placeholder[i].src = response.url
+            })
+         .catch((error) => {
+            console.error('Error:', error);
+            })
+    }
+}
+
+/* when document is fully loaded, renderItem is executed*/
+window.addEventListener('load', () => renderItem());
---
+++ readme.md
@@ -0,0 +1,19 @@
+# Webpage created with Bootstrap
+
+![GitHub Logo](/images/bootstrap.bmp)
+
+
+[Lets go to live preview]()
+
+Not a commercial project. A simple website with a few subpages created only with Bootstrap 4.5, HTML, CSS, and JS. Pictures are fetched by Java Script in script.js. The project was designed and implemented to meet the requirements of Harvard's CS50 course (MOOC).
+
+## Technologies:
+- Bootstrap v4.5
+- CSS
+- HTML
+- JavaScript
+
+## Tools:
+- Visual Studio Code
+- Webpack
+- Git
